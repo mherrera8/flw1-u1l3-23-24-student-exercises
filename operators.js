02879f798541496356ca381e76d5489788bed9b5
mherrera8 <147006626+mherrera8@users.noreply.github.com>
2023-10-23 23:23:07 +0000

    Complete magicalCreature
    Complete magicalCreature

--- operators.js
+++ operators.js
@@ -6,18 +6,25 @@
 // Add the two numbers and store the result in a new variable called sum.
 // Print the result using console.log().
 
+let num1 = 5;
+let num2 = 8;
+let sum = num1 + num2;
 
+console.log(sum);
 
 // 2. Subtraction
 // Subtract num2 from num1 and store the result in a new variable called difference.
 // Print the result.
 
+let difference = num2 - num1;
+console.log(difference);
 
 // 3. Multiplication
 // Multiply num1 by num2 and store the result in a new variable called product.
 // Print the result.
 
-
+let product = num1 * num2;
+console.log(product);
 
 // String Operations
 
@@ -26,3 +33,8 @@
 // Concatenate (combine) the two strings using the + operator and store the result in a new variable called combinedString.
 // Print the result.
 
+let str1 = "Hello ";
+let str2 = "World!";
+let combinedString = str1 + str2;
+
+console.log(combinedString);
